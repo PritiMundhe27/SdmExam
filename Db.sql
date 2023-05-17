b13bfb19a59bd83215aac8ce4c74b96c96e86c71
UddeshDeshmukh21 <uddeshdeshmukh31@gmail.com>
2023-05-17 14:41:26 +0530

    Database

---
+++ Db.sql
@@ -0,0 +1,9 @@
+create table Project(
+project_id int primary key,
+projectName varchar(30),
+ProjectDuration int,
+StartDate date,
+DeliveryDate date,
+ManagerID int,
+DepartmentID int,
+EmpID int);
